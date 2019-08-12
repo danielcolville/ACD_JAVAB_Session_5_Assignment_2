9f0c17e5ff594706fecd9010841d2bdf33e9ae96
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-12 14:42:44 -0400

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -11,16 +11,13 @@ public class Triangle extends Figure {
 	}
 	@Override
 	void findArea() {
-		// TODO Auto-generated method stub
 		double s=(a+b+c)/2;
-		
 		System.out.println("Triangle area is "+Math.sqrt(s*(s-a)*(s-b)*(s-c)));
 		
 	}
 
 	@Override
 	void findPerimeter() {
-		// TODO Auto-generated method stub
 		System.out.println("Triangle perimeter is "+(a+b+c));
 	}
 
